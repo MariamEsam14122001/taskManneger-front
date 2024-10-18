51d7d0c5401ab7383fd1f20064cdf43ce7039169
Sherouk Yasser <yassersherouk9@gmail.com>
2024-10-18 18:41:53 +0300

    reports page responsive handling still there are issues in the middle screen

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import TaskStats from '../../components/Statistics/TaskStats';
 import Dashboard from '../../components/Dashboard/Dashboard';
+import Sidebar from '../../components/sidebar/SideBar';
+import Header from '../../components/Header/Header';
+import ProgressBar from '../../components/Progressbar/progressbar';
+
 
 function Reports() {
 return (
-<div>
-    <TaskStats/>
-    <Dashboard/>
-</div>
-);
+        <div className="app-container">
+            <Sidebar />
+            <main className="main-content">
+            <Header />
+            <div>
+            <ProgressBar current={25} total={50} />
+            <TaskStats/>
+            <Dashboard/>
+            </div>
+            </main>
+        </div>
+        );
+
 }
 
 export default Reports;
