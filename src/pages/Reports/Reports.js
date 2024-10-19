7d539fd3824d3232eae8d68e93b96751ba82d232
MariamEsam14122001 <mariamessamadel@gmail.com>
2024-10-19 19:15:07 +0300

    report

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import TaskStats from '../../components/Statistics/TaskStats';
-import Dashboard from '../../components/Dashboard/Dashboard';
-import Sidebar from '../../components/sidebar/SideBar';
-import Header from '../../components/Header/Header';
-import ProgressBar from '../../components/Progressbar/progressbar';
+import React from "react";
+import TaskStats from "../../components/Statistics/TaskStats";
+import Dashboard from "../../components/Dashboard/Dashboard";
 
+import ProgressBar from "../../components/Progressbar/progressbar";
 
 function Reports() {
-return (
-        <div className="app-container">
-            <Sidebar />
-            <main className="main-content">
-            <Header />
-            <div>
-            <ProgressBar current={25} total={50} />
-            <TaskStats/>
-            <Dashboard/>
-            </div>
-            </main>
-        </div>
-        );
-
+  return (
+    <div>
+      <ProgressBar current={25} total={50} />
+      <TaskStats />
+      <Dashboard />
+    </div>
+  );
 }
 
 export default Reports;
